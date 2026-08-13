f6ade634b4e9718300f911be38ca7ccce6d21b2f
HARSH THAKARE <151563948+harsh21234i@users.noreply.github.com>
2026-08-13 21:49:28 +0530

    added list in js

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const form = document.querySelector("form");
 const input = document.querySelector("#input");
 const taskList = document.querySelector("#list");
 
-console.log(form);
-console.log(input);
-console.log(taskList);
-
 form.addEventListener("submit", function(event) {
     event.preventDefault();
+
     const task = input.value.trim();
-     if (task === "") {
+
+    if (task === "") {
         return;
     }
-    console.log(task);
+
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    taskList.appendChild(li);
+
+    input.value = "";
 });
